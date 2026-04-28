65b7f11b103ebc2363d9ef7533ab140abbee08be
Lunas322 <fpvmxm333477@gmail.com>
2026-04-29 08:15:10 +0900

    feat: title common 분리

---
+++ src/common/Title.tsx
@@ -0,0 +1,10 @@
+
+function Title({text}: {text: string}) {
+    return (
+        <div className="w-15 h-11 flex justify-center items-center font-bold text-xl">
+            <h2>{text}</h2>
+        </div>
+    )
+}
+
+export default Title
