57e71f4934f7b773b6b1c4450c86f39da594041b
Lunas322 <fpvmxm333477@gmail.com>
2026-04-29 12:42:02 +0900

    feat: bannerMockData 제작

---
+++ src/Mock/bannerMockData.ts
@@ -0,0 +1,12 @@
+export const bannerMockData = [
+    {id:1,src:"https://d3ha2047wt6x28.cloudfront.net/qfoiKi-pqOg/pr:RECTANGLE_BANNER/czM6Ly9pbWcuYS1ibHkuY29tL2Jhbm5lci9pbWFnZXMvYmFubmVyX2ltYWdlXzE3NzY5MTI2NzkyNjYwNDYuZ2lm"},
+    {id:2,src:"https://d3ha2047wt6x28.cloudfront.net/S-SyuY7p-kc/pr:RECTANGLE_BANNER/czM6Ly9pbWcuYS1ibHkuY29tL2Jhbm5lci9pbWFnZXMvYmFubmVyX2ltYWdlXzE3NzY5MjAwNzE0MzA0NjcucG5n"},
+    {id:3,src:"https://d3ha2047wt6x28.cloudfront.net/u3DxxnDBL6Y/pr:RECTANGLE_BANNER/czM6Ly9pbWcuYS1ibHkuY29tL2Jhbm5lci9pbWFnZXMvYmFubmVyX2ltYWdlXzE3NzY5MTkwMjIzNjQ2MDcuZ2lm"},
+    {id:4,src:"https://d3ha2047wt6x28.cloudfront.net/6XTK51uAEO4/pr:RECTANGLE_BANNER/czM6Ly9pbWcuYS1ibHkuY29tL2Jhbm5lci9pbWFnZXMvYmFubmVyX2ltYWdlXzE3NzczOTIwMjgxODYyODEuZ2lm"},
+    {id:5,src:"https://d3ha2047wt6x28.cloudfront.net/M84KYt2HRxI/pr:RECTANGLE_BANNER/czM6Ly9pbWcuYS1ibHkuY29tL2Jhbm5lci9pbWFnZXMvYmFubmVyX2ltYWdlXzE3NzY5MTk2MDIzMjUyNzIucG5n"},
+    {id:6,src:"https://d3ha2047wt6x28.cloudfront.net/l0AyneZaXyQ/pr:RECTANGLE_BANNER/czM6Ly9pbWcuYS1ibHkuY29tL2Jhbm5lci9pbWFnZXMvYmFubmVyX2ltYWdlXzE3NzY5Mjg5NDQyODY4OTguZ2lm"},
+    {id:7,src:"https://d3ha2047wt6x28.cloudfront.net/jnAVf_WBvdI/pr:RECTANGLE_BANNER/czM6Ly9pbWcuYS1ibHkuY29tL2Jhbm5lci9pbWFnZXMvYmFubmVyX2ltYWdlXzE3NzY5MTg3MDI1NzUwNDQuZ2lm"},
+    {id:8,src:"https://d3ha2047wt6x28.cloudfront.net/lwZf4pT0ZtY/pr:RECTANGLE_BANNER/czM6Ly9pbWcuYS1ibHkuY29tL2Jhbm5lci9pbWFnZXMvYmFubmVyX2ltYWdlXzE3NzY5MTQzNDA2NTY5ODUucG5n"},
+    {id:9,src:"https://d3ha2047wt6x28.cloudfront.net/Drev6H-iHtQ/pr:RECTANGLE_BANNER/czM6Ly9pbWcuYS1ibHkuY29tL2Jhbm5lci9pbWFnZXMvYmFubmVyX2ltYWdlXzE3NzY4NDI4MjA0NTQ1NzkucG5n"},
+    {id:10,src:"https://d3ha2047wt6x28.cloudfront.net/RrcO3epQqOc/pr:RECTANGLE_BANNER/czM6Ly9pbWcuYS1ibHkuY29tL2Jhbm5lci9pbWFnZXMvYmFubmVyX2ltYWdlXzE3NzY2ODIzMTY5MjU2OTcuanBn"}
+]
