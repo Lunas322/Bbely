653cc00b71121ad39708decf664ae6948b288373
Lunas322 <fpvmxm333477@gmail.com>
2026-04-29 09:07:58 +0900

    fix: css 레이아웃 수정

--- src/common/Title.tsx
+++ src/common/Title.tsx
@@ -1,7 +1,7 @@
 
 function Title({text}: {text: string}) {
     return (
-        <div className="w-15 h-11 flex justify-center items-center font-bold text-xl">
+        <div className="w-auto h-11 flex justify-center items-center font-bold text-lx">
             <h2>{text}</h2>
         </div>
     )
