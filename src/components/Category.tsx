5196535beefda646a31c72e92f3a8d71a169c216
Lunas322 <fpvmxm333477@gmail.com>
2026-04-30 23:07:27 +0900

    refactor: 불필요 div 제거

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -7,13 +7,12 @@ function Category () {
         <div className="w-150 h-25 bg-[#FFFFFF] justify-between flex flex-row p-2 gap-4 mt-1.5">
 
         {CategoryMockData.map((item)=>{
-            return <div className="flex">
-                <div className="flex flex-col justify-between items-center w-15.5 h-14.5 ">
-                <img src={item.img} key={item.id} alt="" className="w-8.5 h-8.5"/>
+            return (
+                <div className="flex flex-col justify-between items-center w-15.5 h-14.5 " key={item.id}>
+                <img src={item.img} alt="" className="w-8.5 h-8.5"/>
                 <p className="text-[11px]">{item.title}</p>
                 </div>
-            </div>
-
+            )
         })}
         </div>
         </>
