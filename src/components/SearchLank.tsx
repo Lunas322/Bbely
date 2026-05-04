40eed7d68d51538f953bc91f145fa2d4d289523b
Lunas322 <fpvmxm333477@gmail.com>
2026-05-05 06:15:24 +0900

    feat: 검색어 랭킹 ui 구현

--- src/components/SearchLank.tsx
+++ src/components/SearchLank.tsx
@@ -8,6 +8,19 @@ function SearchLank () {
         {id:5, searchText:'30대 이상'}
 
     ]
+    const searchRankMockData = [
+  { id: 0, searchRankText: "후드티", rank: 1 },
+  { id: 1, searchRankText: "맨투맨", rank: 2 },
+  { id: 2, searchRankText: "청바지", rank: 3 },
+  { id: 3, searchRankText: "운동화", rank: 4 },
+  { id: 4, searchRankText: "반팔티", rank: 5 },
+  { id: 5, searchRankText: "가디건", rank: 6 },
+  { id: 6, searchRankText: "백팩", rank: 7 },
+  { id: 7, searchRankText: "슬랙스", rank: 8 },
+  { id: 8, searchRankText: "모자", rank: 9 },
+];
+
+
     return(
         <>
         <div className="w-150 h-screen bg-[#FFFFFF] mt-12">
@@ -21,6 +34,19 @@ function SearchLank () {
                     {data.searchText}
                 </div>
             )})}
+        </div>
+        <div className="flex flex-col p-2 mt-2">
+            {searchRankMockData.map((data)=>{
+                return(
+                    <>
+                    <div className="flex justify-start w-full h-9 gap-3 items-center font-black text-sm" key={data.id}>
+                        <div>{data.rank}</div>
+                        <div className="font-medium text-xs">{data.searchRankText}</div>
+
+                    </div>
+                    </>
+                )
+            })}
         </div>
         </div>
   
