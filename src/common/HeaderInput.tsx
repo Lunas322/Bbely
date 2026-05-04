d0478b25e5ff66d6f29bd96d526036c71dbf99db
Lunas322 <fpvmxm333477@gmail.com>
2026-05-04 12:18:31 +0900

    feat: searchPage 이동 함수 추가

--- src/common/HeaderInput.tsx
+++ src/common/HeaderInput.tsx
@@ -1,12 +1,19 @@
 import { IoIosSearch } from "react-icons/io"
+import { useNavigate } from "react-router-dom"
 
 function HeaderHome({disabled}:{disabled?: boolean}) {
+    const nav = useNavigate()
+    function moveSearch () {
+        if (disabled) {
+            nav('/search')
+        }
+    }
     return (
         <>
                 <div className="w-auto h-9.5 bg-[#EEEEEE] flex items-center mx-auto gap-2 px-2 rounded-md">
                     <div className="w-127 h-9.5 bg-[#EEEEEE] flex items-center gap-2 px-2">
                     <IoIosSearch className="w-5 h-5 text-[#777777]" />
-                    <input type="text" placeholder="하나만 사도 무료배송" className="w-100 outline-0 " disabled={disabled} />
+                    <input type="text" placeholder="하나만 사도 무료배송" className="w-100 outline-0 " readOnly={disabled} onClick={moveSearch}/>
                     </div>
                 </div>
                 
