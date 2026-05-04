05a8c9d4542e1986e0408a85508092825f3dba95
Lunas322 <fpvmxm333477@gmail.com>
2026-05-04 11:48:25 +0900

    feat: header Page타입 선택 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,14 @@
 import HeaderTop from "../common/HeaderTop"
 import HeaderSub from "../common/HeaderSub"
 import { PiShoppingCartLight } from "react-icons/pi"
+import { HeaderSubProps } from "../types/header"
 
-function Header() {
+function Header(Page:HeaderSubProps) {
   return (
     <div className="w-150 h-27 fixed top-0 z-20 bg-[#FFFFFF]">
         <HeaderTop/>
       <div className="w-150 h-12 flex items-center justify-between ">
-        <HeaderSub icons={[PiShoppingCartLight]} Page="home"/>
+        <HeaderSub icons={[PiShoppingCartLight]} Page={Page.Page}/>
       </div>
       
     </div>
