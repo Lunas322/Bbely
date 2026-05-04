eb4a9a6fcfe696849db79fbdd9a716c43172afe7
Lunas322 <fpvmxm333477@gmail.com>
2026-05-04 12:19:17 +0900

    feat: 각 메뉴 이동 함수 추가

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,5 +1,6 @@
-import { useState } from "react"
+
 import { IoIosSearch, IoMdHome, IoMdPerson } from "react-icons/io"
+import { useLocation, useNavigate } from "react-router-dom"
 
 //menuBar가 해야할것 1 누르면 색 변하기
 //눌렀을때 다른 메뉴는 색이 없어야함.
@@ -7,22 +8,22 @@ import { IoIosSearch, IoMdHome, IoMdPerson } from "react-icons/io"
 
 function MenuBar () {
     const menuData = [
-        {id:1, label:'홈', icon:<IoMdHome className="w-5 h-5"/>},
-        {id:2, label:'검색', icon:<IoIosSearch className="w-5 h-5"/>},
-        {id:3, label:'마이페이지',icon:<IoMdPerson className="w-5 h-5"/>}
+        {id:1, label:'홈', path:'/', icon:<IoMdHome className="w-5 h-5"/>},
+        {id:2, label:'검색', path:'/search', icon:<IoIosSearch className="w-5 h-5"/>},
+        {id:3, label:'마이페이지',path:'/my',icon:<IoMdPerson className="w-5 h-5"/>}
     ]
 
-    const [active,setActive] = useState('홈')
-
+    const location = useLocation()
+    const nav = useNavigate()
     
     return (
         <>
         <div className="w-150 h-13 grid grid-cols-3 z-10  fixed bottom-0 mt-26 bg-[#FFFFFF]">
         {menuData.map((data)=>{
-            const isActive = data.label === active ? 'text-[#FF5160]' : 'text-[#777777]'
+            const isActive = data.path === location.pathname ? 'text-[#FF5160]' : 'text-[#777777]'
             return (
                        <div className={`${isActive} flex justify-center items-center flex-col col-span-1`}
-                       onClick={()=>setActive(data.label)}
+                       onClick={(()=>{nav(data.path)})}
                        >
                         {data.icon}
                         <p className="text-xs">{data.label}</p>
