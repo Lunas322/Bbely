f79abaef6b3e826062f5cd433cf84ebb2f19f7fe
Lunas322 <fpvmxm333477@gmail.com>
2026-05-08 07:23:58 +0900

    feat: search Page 기능 구현, 조건부 렌더링

--- src/Pages/Search.tsx
+++ src/Pages/Search.tsx
@@ -1,15 +1,20 @@
+import { useState } from "react";
 import Header from "../components/Header";
 import MenuBar from "../components/MenuBar";
 import RecentSearch from "../components/RecentSearch";
 import SearchLank from "../components/SearchLank";
+import ItemContainer from "../components/ItemList/ItemContainer";
 
 function Search () {
+    const [search, setSearch] = useState('')
+    console.log(search)
     return(
         <>
         <div className="w-full h-screen flex flex-col items-center">
-            <Header Page="search" />
-            <RecentSearch/>
-            <SearchLank/>
+            <Header Page="search" setSearch={setSearch}/>
+            {search ?
+            <div className="pt-27"><ItemContainer search={search}/></div>:<div><RecentSearch/> <SearchLank/></div>
+            }
             <MenuBar/>
         </div>
         </>
