703ae4229035062728d61f05f27c147b1e7a532a
Lunas322 <fpvmxm333477@gmail.com>
2026-05-10 23:03:46 +0900

    feat: home nav 추가

--- src/common/HeaderTop.tsx
+++ src/common/HeaderTop.tsx
@@ -1,8 +1,15 @@
+import { useNavigate } from "react-router-dom"
+
 function   HeaderTop() {
-    return (
+  const nav = useNavigate()
+
+  function MoveHome () {
+    return nav('/')
+  }
+     return (
            <div className="w-150 h-15 bg-[#FF5160] justify-between font-bold flex items-center px-2.5 py-3 ">
         <div className="w-55 h-9 flex items-center gap-2">
-        <div className="w-8.75 h-8.75 font-black text-[9px] flex justify-center items-center rounded-md bg-[#FFFFFF]">B B L Y</div>
+        <div className="w-8.75 h-8.75 font-black text-[9px] flex justify-center items-center rounded-md bg-[#FFFFFF]" onClick={MoveHome}>B B L Y</div>
         
         <p className="text-[10px] text-[#FFFFFF] font-bold w-45 h-4">앱에서 더 많은 상품을 볼 수 있어요!</p>
         </div>
