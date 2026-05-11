f1a4e75b19cafb9e9d2b11b3c9978c28a04cb385
Lunas322 <fpvmxm333477@gmail.com>
2026-05-11 21:36:44 +0900

    refactor: mock data type 수정

--- src/Mock/itemMockData.ts
+++ src/Mock/itemMockData.ts
@@ -4,7 +4,7 @@ export const itemMockData = [
     shop:'프레첼',
     itemTitle:'가멘츠 레터링 밴딩 팬츠',
     discount:49,
-    price:'49,800',
+    price:49800,
     id:0,
     color:['블랙','그레이','네이비'],
     size:['M','L','XL']
@@ -15,7 +15,7 @@ export const itemMockData = [
     shop:'무드멜트',
     itemTitle:'헤비 카고 버뮤다팬츠 와이드 반바지 스웻팬츠',
     discount:null,
-    price:'34,560',
+    price:34560,
     id:1,
     color:['카키','블랙','차콜'],
     size:['FREE']
@@ -26,7 +26,7 @@ export const itemMockData = [
     shop:'쉐딩핏',
     itemTitle:'크라운 레드 오버 후드집업',
     discount:11,
-    price:'33,800',
+    price:33800,
     id:2,
     color:['레드','블랙'],
     size:['M','L']
@@ -37,7 +37,7 @@ export const itemMockData = [
     shop:'프레첼',
     itemTitle:'도넌 스타 레터링 와이드 롱팬츠',
     discount:52,
-    price:'32,400',
+    price:32400,
     id:3,
     color:['블랙','화이트'],
     size:['S','M','L']
@@ -48,7 +48,7 @@ export const itemMockData = [
     shop:'프라운',
     itemTitle:'버뮤다 와이드 벌룬 반바지 핀턱',
     discount:27,
-    price:'12,250',
+    price:12250,
     id:4,
     color:['베이지','블랙','아이보리'],
     size:['M','L']
@@ -59,7 +59,7 @@ export const itemMockData = [
     shop:'헤이나나',
     itemTitle:'카펜더 핀턱 와이드 데님',
     discount:26,
-    price:'32,500',
+    price:32500,
     id:5,
     color:['연청','중청','흑청'],
     size:['S','M','L','XL']
@@ -70,7 +70,7 @@ export const itemMockData = [
     shop:'초코송이무슨맛',
     itemTitle:'이거 입으면 나도 래퍼여친!',
     discount:40,
-    price:'32,980',
+    price:32980,
     id:6,
     color:['핑크','블랙'],
     size:['FREE']
@@ -81,7 +81,7 @@ export const itemMockData = [
     shop:'토마토스터디',
     itemTitle:'토마토스터디 필수템!',
     discount:11,
-    price:'22,300',
+    price:22300,
     id:7,
     color:['화이트','네이비'],
     size:['M','L']
@@ -92,7 +92,7 @@ export const itemMockData = [
     shop:'역사전문',
     itemTitle:'이것만 있으면 나도 꽁치!',
     discount:35,
-    price:'42,000',
+    price:42000,
     id:8,
     color:['그린','블랙'],
     size:['95','100','105']
@@ -103,7 +103,7 @@ export const itemMockData = [
     shop:'몽요즈',
     itemTitle:'트랙바람막이! 봄-가을까지 쭉가능',
     discount:null,
-    price:'25,000',
+    price:25000,
     id:9,
     color:['화이트','블루','블랙'],
     size:['FREE']
@@ -114,7 +114,7 @@ export const itemMockData = [
     shop:'영국남자',
     itemTitle:'거기 조교님 이거 한번 써보슈',
     discount:20,
-    price:'5,500',
+    price:5500,
     id:10,
     color:['브라운','블랙'],
     size:['FREE']
