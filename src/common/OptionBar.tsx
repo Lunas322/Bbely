042db65076dad08401f44ae87ed4fb358f4aa95e
Lunas322 <fpvmxm333477@gmail.com>
2026-05-12 01:42:33 +0900

    feat: option 컴포넌트 구조 변경

--- src/common/OptionBar.tsx
+++ src/common/OptionBar.tsx
@@ -1,30 +1,17 @@
-import React from "react"
-type selectCase = 'color' | 'size'
-type select = {
-    color: string
-    size: string
-}
-
 type OptionBarProps = {
   text: string
-  setSelect: React.Dispatch<React.SetStateAction<select>>
-  Case: selectCase
+  onSelect: (value: string) => void
 }
 
-
-
-function OptionBar ({text, setSelect, Case}:OptionBarProps) {
-
-    function selectData () {
-    Case === 'color' ? setSelect((prev)=> ({...prev, color: text})) : setSelect((prev)=> ({...prev, size: text}))
+function OptionBar({ text, onSelect }: OptionBarProps) {
+  return (
+    <div
+      className="w-140 h-14 text-sm border border-[#7777774f] rounded-sm flex items-center p-2"
+      onClick={() => onSelect(text)}
+    >
+      {text}
+    </div>
+  )
 }
 
-    return <>
-                <div className='w-140 h-14  text-sm border border-[#7777774f] rounded-sm flex justify-between items-center p-2'
-                onClick={()=>{selectData()}}
-                >
-                        {text}
-                </div>
-    </>
-}
 export default OptionBar
