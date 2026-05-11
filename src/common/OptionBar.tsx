3632255a0738af9fbd6221f6433b6425995400ea
Lunas322 <fpvmxm333477@gmail.com>
2026-05-11 11:05:08 +0900

    feat: 상품 선택 함수 추가

--- src/common/OptionBar.tsx
+++ src/common/OptionBar.tsx
@@ -1,10 +1,28 @@
-type OptionBarProps = {
-  text: string
+import React from "react"
+type selectCase = 'color' | 'size'
+type select = {
+    color: string
+    size: string
 }
 
-function OptionBar ({text}:OptionBarProps) {
+type OptionBarProps = {
+  text: string
+  setSelect: React.Dispatch<React.SetStateAction<select>>
+  Case: selectCase
+}
+
+
+
+function OptionBar ({text, setSelect, Case}:OptionBarProps) {
+
+    function selectData () {
+    Case === 'color' ? setSelect((prev)=> ({...prev, color: text})) : setSelect((prev)=> ({...prev, size: text}))
+}
+
     return <>
-                <div className='w-140 h-14  text-sm border border-[#7777774f] rounded-sm flex justify-between items-center p-2'>
+                <div className='w-140 h-14  text-sm border border-[#7777774f] rounded-sm flex justify-between items-center p-2'
+                onClick={()=>{selectData()}}
+                >
                         {text}
                 </div>
     </>
