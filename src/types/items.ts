2dca40dce0a8a7b810c10b9ee77df5f08c1019ed
Lunas322 <fpvmxm333477@gmail.com>
2026-05-14 17:23:23 +0900

    feat: item types 파일 분리

--- src/types/items.ts
+++ src/types/items.ts
@@ -14,4 +14,20 @@ export type modalOption = {
     color?: string[]
     size?: string[]
     price?: number
+    img?: string
+    cartData: Item[]
+    setCartData: React.Dispatch<React.SetStateAction<Item[]>>
+}
+
+export type Item = {
+  size?: string;
+  color?: string;
+  count: number;
+  img?: string;
+  price?: number;
+};
+
+export type cartDataProps = {
+    cartData: Item[]
+    setCartData: React.Dispatch<React.SetStateAction<Item[]>>
 }
