d6fd4310eb1d2f6d00047c1f38dc9a3cc467384e
Lunas322 <fpvmxm333477@gmail.com>
2026-05-17 19:40:09 +0900

    feat: item type 변경

--- src/types/items.ts
+++ src/types/items.ts
@@ -10,22 +10,32 @@ export type itemListCardProps = {
     id:number
 }
 
+export type SelectedItem = {
+  size: string
+  color: string
+  count: number
+}
+
 export type modalOption = {
-    color?: string[]
-    size?: string[]
-    price?: number
-    img?: string
-    cartData: Item[]
-    itemTitle?: string
-    setCartData: React.Dispatch<React.SetStateAction<Item[]>>
+  color?: string[]
+  size?: string[]
+
+  price: number
+  img: string
+  itemTitle: string
+
+  cartData: Item[]
+  setCartData: React.Dispatch<React.SetStateAction<Item[]>>
 }
 
 export type Item = {
-  size?: string;
-  color?: string;
+  size: string;
+  color: string;
   count: number;
-  img?: string;
-  price?: number;
+  img: string;
+  price: number;
+  id:number;
+  itemTitle:string
 };
 
 export type cartDataProps = {
